a9bb4ecaec3e1c9c543a100f5fc400ce309ec89b
Michelangelo Camaioni <42435587+Genio2003@users.noreply.github.com>
2021-11-07 01:17:54 +0100

    Added a Time Based Dark Theme Script!

--- Theme_Switcher.js
+++ Theme_Switcher.js
@@ -1,30 +1,53 @@
-/* Genio2003.tk Theme_Switcher */
+/* Genio2003.tk - The_Theme_Master */
 
-function Theme_Switcher(Theme_Name) {
-	var element = document.body;
-	switch (Theme_Name) {
-		case 'dark_theme':
-			element.classList.toggle("dark-theme");
-			if (element.className == 'dark-theme') {
-				document.getElementById("Halloween_Theme_Switch").style.display = "none";
-			}
-			else {
-				document.getElementById("Halloween_Theme_Switch").style.display = "inline-block";
-			}
-			break;
-		case 'halloween_theme':
-			element.classList.toggle("halloween-theme");
-			if (document.getElementById("Emoji").innerHTML != "🎃") {
-				document.getElementById("Gif").src = "https://media.giphy.com/media/ecP7TG62hZb800WiBe/giphy.gif";
-				document.getElementById("Emoji").innerHTML = "🎃";
-				document.getElementById("Dark_Theme_Switch").style.display = "none";
-			}
-			else {
-				document.getElementById("Gif").src = "https://media.giphy.com/media/gw3IWyGkC0rsazTi/giphy.gif";
-				document.getElementById("Emoji").innerHTML = "❤️";
-				document.getElementById("Dark_Theme_Switch").style.display = "inline-block";
-			}
-			break;
-		default:
+var element = document.body;
+
+function Theme_Master() {
+	var Time = new Date().getHours();
+	var Day = new Date().getDate();
+	var Month = new Date().getMonth();
+	if (Month == 9 && Day == 31) {
+		Halloween_Theme();
+	}
+	else {
+		if (Time >= 20 || Time < 7) {
+		Dark_Theme();
 		}
+	}
+}
+
+function Dark_Theme() {
+	element.classList.toggle("dark-theme");
+	if (element.className == 'dark-theme') {
+		document.getElementById("Halloween_Theme_Switch").style.display = "none";
+	}
+	else {
+		document.getElementById("Halloween_Theme_Switch").style.display = "inline-block";
+	}
+}
+
+function Halloween_Theme() {
+	element.classList.toggle("halloween-theme");
+	if (document.getElementById("Emoji").innerHTML != "🎃") {
+		document.getElementById("Gif").src = "https://media.giphy.com/media/ecP7TG62hZb800WiBe/giphy.gif";
+		document.getElementById("Emoji").innerHTML = "🎃";
+		document.getElementById("Dark_Theme_Switch").style.display = "none";
+	}
+	else {
+		document.getElementById("Gif").src = "https://media.giphy.com/media/gw3IWyGkC0rsazTi/giphy.gif";
+		document.getElementById("Emoji").innerHTML = "❤️";
+		document.getElementById("Dark_Theme_Switch").style.display = "inline-block";
+	}
+}
+
+function Override(Theme_Name) {
+	switch (Theme_Name) {
+	case 'dark_theme':
+		Dark_Theme()
+		break;
+	case 'halloween_theme':
+		Halloween_Theme()
+		break;
+	default:
+	}
 }
