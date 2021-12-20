9f07b71d7ce0aa56e79a2b64c7c1fa1dd100e94b
Michelangelo Camaioni <42435587+Genio2003@users.noreply.github.com>
2021-12-20 14:44:11 +0100

    Added Responsive Design

--- Content_Manager.js
+++ Content_Manager.js
@@ -5,19 +5,19 @@ var element = document.body;
 function Content_Switcher(Sel) {
 	switch (Sel) {
 	case '0':
-		document.getElementById("Home").style.display = "inline-block";
+		document.getElementById("Home").style.display = "grid";
 		document.getElementById("Projects").style.display = "none";
 		document.getElementById("Contacts").style.display = "none";
 		break;
 	case '1':
 		document.getElementById("Home").style.display = "none";
-		document.getElementById("Projects").style.display = "inline-block";
+		document.getElementById("Projects").style.display = "grid";
 		document.getElementById("Contacts").style.display = "none";
 		break;
 	case '2':
 		document.getElementById("Home").style.display = "none";
 		document.getElementById("Projects").style.display = "none";
-		document.getElementById("Contacts").style.display = "inline-block";
+		document.getElementById("Contacts").style.display = "grid";
 		break;
 	default:
 	}
